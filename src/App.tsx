c5059707cc4381f7ec460fa4abf8a40c0863b9f0
Renato Freire <contato.renatofreire@gmail.com>
2021-02-07 18:24:53 -0300

    Install react-router-dom and create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link, Route, Switch } from "react-router-dom";
+import ROUTES from "./constants/routes";
 
 function App() {
   return (
@@ -6,6 +8,20 @@ function App() {
       <header>
         <p>Poke API</p>
       </header>
+      <nav>
+        <Link to={ROUTES.POKEDEX}>Pokedex</Link>
+        <Link to={ROUTES.SEARCH_POKEMON}>Search Pokemon</Link>
+      </nav>
+      <div>
+        <Switch>
+          <Route exact path={ROUTES.POKEDEX}>
+            <div>Route 1</div>
+          </Route>
+          <Route exact path={ROUTES.SEARCH_POKEMON}>
+            <div>Route 2</div>
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 }
---
+++ src/constants/routes.ts
@@ -0,0 +1,8 @@
+import { RouteProps } from "react-router-dom";
+
+const ROUTES = {
+  POKEDEX: "/pokedex",
+  SEARCH_POKEMON: "/search-pokemon",
+};
+
+export default ROUTES;
